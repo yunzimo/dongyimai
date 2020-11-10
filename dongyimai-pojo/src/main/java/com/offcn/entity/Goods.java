f9358be910f4040e65121d1e9a2296728be7642b
yunzimo <yunzimolikeyou@outlook.com>
2020-11-10 10:52:50 +0800

    添加部分注释

---
+++ dongyimai-pojo/src/main/java/com/offcn/entity/Goods.java
@@ -0,0 +1,13 @@
+package com.offcn.entity;
+
+import com.offcn.pojo.TbGoods;
+import com.offcn.pojo.TbGoodsDesc;
+
+import java.io.Serializable;
+
+public class Goods implements Serializable {
+
+    private TbGoods goods;
+    private TbGoodsDesc goodsDesc;
+
+}
--- dongyimai-shop-web/src/main/java/com/offcn/shop/ServiceImp/UserDetailServiceImp.java
+++ dongyimai-shop-web/src/main/java/com/offcn/shop/ServiceImp/UserDetailServiceImp.java
@@ -21,6 +21,10 @@ public class UserDetailServiceImp implements UserDetailsService {
 
     private SellerService sellerService;
 
+    // sellerService是可以用@reference注解自动导入的，只需要在spring-sercurity配置文件里配置dubbo扫描就行了，
+    // 注意这里的dubbo扫描不可以设置在springmvc配置文件中，在web.xml文件中springmvc配置文件的引入时机是在dispatcherServlet触发的时候
+    // 当直接访问网页的时候会因为读取不到配置而导致验证的时候的空指针
+
     public SellerService getSellerService() {
         return sellerService;
     }
